5ea18faa85787a59cc1af0ba19643d021e223bd2
JACK99-JACK99 <51847087+JACK99-JACK99@users.noreply.github.com>
2022-02-21 21:57:18 +0530

    files name are modidied

--- quickstart/views.py
+++ quickstart/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import UserSerializer, GroupSerializer
-# Create your views here.
+# Create your views here.d
 
 
 class UserViewset(viewsets.ModelViewSet):
